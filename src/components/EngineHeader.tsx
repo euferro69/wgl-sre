3ffe6711d35f82d9f44d5bef410217192f5bcd52
breno <baferro@ljcti.net.br>
2025-01-25 10:48:21 -0300

    Refactor: UI changes

--- src/components/EngineHeader.tsx
+++ src/components/EngineHeader.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function EngineHeader() {
   return (
     <div className="flex flex-col w-full">
-      <div className="flex flex-row justify-start align-middle bg-[#222] border border-t-[#333] border-b-[#333] border-l-0 border-r-0">
+      {/* <div className="flex flex-row justify-start align-middle bg-[#222] border border-t-[#333] border-b-[#333] border-l-0 border-r-0">
         <img
           alt="Blue Engine"
           src="/favicon.ico"
@@ -13,23 +13,32 @@ export default function EngineHeader() {
           height={24}
           className="object-contain ml-2 mr-1"
         />
+        Blue Engine
         <Tooltip title="File actions">
           <button className="px-2 py-1 mr-2 hover:bg-slate-600">File</button>
         </Tooltip>
         <button className="px-2 py-1 mr-2 hover:bg-slate-600">Edit</button>
         <button className="px-2 py-1 mr-2 hover:bg-slate-600">View</button>
         <Link href={"https://github.com/euferro69/BlueEngine"}><button className="px-2 py-1 mr-2 hover:bg-slate-600">Docs</button></Link>
-      </div>
+      </div> */}
       <div className="flex flex-row w-full bg-[#222] p-2 border border-t-[#333] border-b-[#333] border-l-0 border-r-0">
         <Button
           variant="contained"
           color="info"
           size="small"
+          className="mr-2"
           onClick={() => {
             playAudio("/audio/ci-3-91252.mp3");
           }}
         >
           Play Audio
+        </Button>
+        <Button
+          variant="contained"
+          color="warning"
+          size="small"
+        >
+          Simulate
         </Button>
       </div>
     </div>
